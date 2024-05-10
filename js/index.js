24cf04be872ed25388929dc420635210c9bd1efb
zhenhongswell <zhenhongswell@gmail.com>
2024-05-10 15:19:20 +0800

    [js]Add load-more button logic

--- js/index.js
+++ js/index.js
@@ -6,13 +6,16 @@ import {
   renderOptions,
   clearImages,
   addSelectOrderListener,
+  addLoadMoreButtonListener,
+  enableLoadMoreButton,
+  disableLoadMoreButton,
 } from "./dom.js";
 
 const catList = [];
+const pagesize = 12;
 let page = 1;
 let order = "DESC";
 let selectOptions = [];
-let limit = 5;
 async function loadCats(limit, page, order, breedIds = []) {
   // 從api抓貓的資料
   let list = await fetchCats(limit, page, order, breedIds);
@@ -25,9 +28,16 @@ async function loadCats(limit, page, order, breedIds = []) {
 
   //   console.log(catList);
   renderCats(list);
+
+  if (list.length < limit) {
+    //no more cat...
+    disableLoadMoreButton();
+    return false;
+  }
+  return true;
 }
 
-function handleBreedOptionChange(event) {
+async function handleBreedOptionChange(event) {
   // TODO
 
   const changedOption = event.target;
@@ -40,7 +50,13 @@ function handleBreedOptionChange(event) {
     );
   }
   clearImages();
-  loadCats(limit, page, order, selectOptions);
+  //過濾條件改變 => 重新啟用loadmoreButton,並將page 還原預設值1
+  enableLoadMoreButton();
+  page = 1;
+  const hasNextPage = await loadCats(pagesize, page, order, selectOptions);
+  if (hasNextPage) {
+    page++;
+  }
 }
 
 async function loadBreedOptions() {
@@ -52,15 +68,30 @@ async function loadBreedOptions() {
 function addListeners() {
   addDropListener();
   addCloseDropdownListener();
-  addSelectOrderListener((e) => {
+  addSelectOrderListener(async (e) => {
     order = e.target.value;
+
     clearImages();
-    loadCats(limit, page, order, selectOptions);
+    //更改順序,重新啟用loadmoreButton,page設預設值 1
+    enableLoadMoreButton();
+    page = 1;
+    const hasNextPage = await loadCats(pagesize, page, order, selectOptions);
+    if (hasNextPage) {
+      page++;
+    }
+  });
+  addLoadMoreButtonListener(async () => {
+    const hasNextPage = await loadCats(pagesize, page, order, selectOptions);
+    if (hasNextPage) {
+      page++;
+    }
   });
 }
 document.addEventListener("DOMContentLoaded", async () => {
   await loadBreedOptions();
-  await loadCats(limit, page, order, selectOptions);
-
+  const hasNextPage = await loadCats(pagesize, page, order, selectOptions);
+  if (hasNextPage) {
+    page++;
+  }
   addListeners();
 });
